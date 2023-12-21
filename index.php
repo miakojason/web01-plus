90c881621c1be86146351d89204484985d6e1620
miakojason <miakojason@gmail.com>
2023-12-21 16:08:38 +0800

    刪除行內bgc控制，更改class控制bgc

--- index.php
+++ index.php
@@ -139,8 +139,8 @@
 						</div>
 					</div>
 					<div style="clear:both;"></div>
-					<div class="col-12"><!--尾-->
-						<div style="width:100%; left:0px;  position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
+					<div class="col-12 bg-secondary"><!--尾-->
+						<div style="width:100%; left:0px;  position:relative; margin-top:4px; height:123px; display:block;">
 							<span class="t " style="line-height:123px;"><?= $Bottom->find(1)['bottom']; ?></span>
 						</div>
 					</div>
