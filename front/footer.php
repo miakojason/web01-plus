efdd4bd4374ab9d642f81b7770166c5e698764d8
miakojason <miakojason@gmail.com>
2024-01-18 08:22:46 +0800

    美化

--- front/footer.php
+++ front/footer.php
@@ -1,9 +1,9 @@
 <div style="clear:both;"></div>
-	<div class="container-fulid bg-secondary " style="height: 10vh;"><!--尾-->
-		<div class="row d-flex">
-			<div class="t col-6">
+	<div class="container-fulid  " ><!--尾-->
+		<div class="row d-flex"style="height: 15vh;">
+			<div class="t col-6 bg-secondary ">
 				進站總人數 :<?= $Total->find(1)['total']; ?></div>
-			<div class="t col-6 "><?= $Bottom->find(1)['bottom']; ?></div>
+			<div class="t col-6 bg-warning"><?= $Bottom->find(1)['bottom']; ?></div>
 		</div>
 	</div>
 	</div>
